9567f55ef29185395ad1543c5f1d23d7b470cd6a
Scott Nelson <snelson@instructors.2u.com>
2023-09-14 13:01:31 -0700

    added PUT and DELETE routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const winston = require("winston");
+const url = require("url");
+const querystring = require("querystring");
 
 const groceryItems = [];
 
@@ -11,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 http
   .createServer((request, response) => {
     try {
-      const { headers, method, url } = request;
+      const { headers, method } = request;
 
       // Log incoming request
       logger.info(`${method} request send to endpoint ${url}`);
@@ -30,7 +32,7 @@ http
 
           // GET
           // Returns all grocery list items
-          if (method === "GET" && url === "/api/groceries") {
+          if (method === "GET" && request.url === "/api/groceries") {
             response.writeHead(200, {
               "Content-Type": "application/json",
             });
@@ -39,7 +41,7 @@ http
 
           // POST
           // Creates a new grocery list item
-          if (method === "POST" && url === "/api/groceries") {
+          if (method === "POST" && request.url === "/api/groceries") {
             let item = JSON.parse(body);
             groceryItems.push(item);
             logger.info("New grocery item added.");
@@ -52,8 +54,59 @@ http
               })
             );
           }
+
+          // PUT
+          // Update a grocery list item
+          if (method === "PUT") {
+            const parsed = url.parse(request.url);
+            const query  = querystring.parse(parsed.query);
+            if(!query.name || groceryItems.length < 1){
+                throw new Error("Bad request");
+            }
+
+            for(let i = 0; i < groceryItems.length; i++){
+                if(groceryItems[i].name === query.name){
+                    let item = JSON.parse(body);
+                    groceryItems.splice(i, 1, item);
+                }
+            }
+
+            logger.info("Successfully updated item.");
+            response.writeHead(201, {
+                "Content-Type": "application/json",
+            });
+            response.end(JSON.stringify({"message": "Successfully updated grocery item"}));
+          }
+
+          // DELETE
+          // Remove an item from the grocery list
+          if (method === "DELETE") {
+            const parsed = url.parse(request.url);
+            const query  = querystring.parse(parsed.query);
+            if(!query.name || groceryItems.length < 1){
+                throw new Error("Bad request");
+            }
+
+            for(let i = 0; i < groceryItems.length; i++){
+                if(groceryItems[i].name === query.name){
+                    groceryItems.splice(i, 1);
+                }
+            }
+            logger.info("Successfully deleted item.");
+            response.writeHead(201, {
+                "Content-Type": "application/json",
+            });
+            response.end(JSON.stringify({"message": "Successfully deleted grocery item"}));
+          }
+
         });
-    } catch (error) {}
+    } catch (error) {
+      logger.error(error.message);
+      response.writeHead(400, {
+        "Content-Type": "application/json",
+      });
+      response.end(JSON.stringify({ message: "Bad Request" }));
+    }
   })
   .listen(PORT, () => {
     logger.info(
